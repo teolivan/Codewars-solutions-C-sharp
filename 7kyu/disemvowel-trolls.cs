96016d5aa4c033e589a9d7e293cadf2ec6663623
olivia svensson <oliviateoliva@gmail.com>
2023-08-19 22:50:51 +0200

    add solution

---
+++ 7kyu/disemvowel-trolls.cs
@@ -0,0 +1,26 @@
+/*
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+*/
+
+//My solution
+
+using System;
+using System.Linq;
+public static class Kata
+{
+    public static string Disemvowel(string str)
+    {
+        string vowels = "AaEeIiOoUu";
+
+str = new string(str.Where(c => !vowels.Contains(c)).ToArray());
+      return str;
+    }
+}
