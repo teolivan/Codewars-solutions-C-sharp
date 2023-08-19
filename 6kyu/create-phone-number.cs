cfca382caecd88a8c641097c43b2558c6c56b594
olivia svensson <oliviateoliva@gmail.com>
2023-08-20 00:02:07 +0200

    add solution

---
+++ 6kyu/create-phone-number.cs
@@ -0,0 +1,17 @@
+/*Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+Example
+Kata.CreatePhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}) // => returns "(123) 456-7890"
+The returned format must be correct in order to complete this challenge.
+
+Don't forget the space after the closing parentheses!*/
+
+//My solution
+
+public class Kata
+{
+  public static string CreatePhoneNumber(int[] numbers)
+  {
+    return "(" + numbers[0].ToString() + numbers[1].ToString() + numbers[2].ToString() + ") " + numbers[3].ToString() + numbers[4].ToString() + numbers[5].ToString() + "-" + numbers[6].ToString() + numbers[7].ToString() + numbers[8].ToString() + numbers[9].ToString();     
+  }
+}
