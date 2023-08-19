91891690aa30eb9283d5e237ad2578c7d787fe6c
olivia svensson <oliviateoliva@gmail.com>
2023-08-20 00:20:09 +0200

    add solution

---
+++ 6kyu/bit-counting.cs
@@ -0,0 +1,23 @@
+/*Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case*/
+
+//My solution
+
+using System;
+
+public class Kata
+{
+  public static int CountBits(int n)
+  {
+int count = 0;
+        
+        while (n > 0)
+        {
+            count += n & 1;
+            n >>= 1;
+        }
+        
+        return count;
+  }
+}
