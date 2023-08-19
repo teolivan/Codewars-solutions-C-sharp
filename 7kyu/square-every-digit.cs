c461a86435aa1d3a8038b212eadca4979780a675
olivia svensson <oliviateoliva@gmail.com>
2023-08-19 23:01:11 +0200

    Add solution

---
+++ 7kyu/square-every-digit.cs
@@ -0,0 +1,33 @@
+/*
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+
+Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
+
+Note: The function accepts an integer and returns an integer.
+
+Happy Coding!
+*/
+
+//My solution
+
+using System;
+
+public class Kata
+{
+  public static int SquareDigits(int n)
+  {
+    var value = n.ToString();
+
+            var result = "";
+
+            for (var i = 0; i < value.Length; i++)
+            {
+                char c = value[i];
+                int num = int.Parse(c.ToString());
+                result += (num * num);
+            }
+            return int.Parse(result);
+  }
+}
