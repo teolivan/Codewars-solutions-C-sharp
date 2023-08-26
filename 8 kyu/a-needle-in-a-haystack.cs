e33d92d49f893ce6b137dcb1524bdadf4cd07853
olivia svensson <oliviateoliva@gmail.com>
2023-08-26 22:12:19 +0200

    add solution

---
+++ 8 kyu/a-needle-in-a-haystack.cs
@@ -0,0 +1,30 @@
+/*Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+After your function finds the needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle, so:
+
+Example(Input --> Output)
+
+["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+Note: In COBOL, it should return "found the needle at position 6"*/
+
+//My solution
+
+using System;
+public class Kata
+{
+  public static string FindNeedle(object[] haystack)
+  {
+   for(int i = 0; i < haystack.Length; i++) {
+     if(haystack[i] == "needle") {
+       return "found the needle at position " + i.ToString();
+     }
+     
+     
+   }
+    return null;
+  }
+  }
