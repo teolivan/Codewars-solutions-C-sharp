9c3e836991b1c3a8af8634d60e94fef3df58e497
olivia svensson <oliviateoliva@gmail.com>
2023-08-26 15:42:45 +0200

    add soluntion

---
+++ 6kyu/array-dif.cs
@@ -0,0 +1,30 @@
+/*Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b keeping their order.
+
+Kata.ArrayDiff(new int[] {1, 2}, new int[] {1}) => new int[] {2}
+If a value is present in b, all of its occurrences must be removed from the other:
+
+Kata.ArrayDiff(new int[] {1, 2, 2, 2, 3}, new int[] {2}) => new int[] {1, 3}*/
+
+//My solution
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+  public class Kata
+{
+  public static int[] ArrayDiff(int[] a, int[] b)
+  {
+     List<int> result = new List<int>();
+
+        foreach (int num in a)
+        {
+            if (!b.Contains(num))
+                result.Add(num);
+        }
+
+        return result.ToArray();
+  }
+}
