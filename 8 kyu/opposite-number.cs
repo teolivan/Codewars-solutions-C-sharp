159b302a342d37b63d2af0c820c8e5cfaea39bfc
olivia svensson <oliviateoliva@gmail.com>
2023-08-26 20:51:39 +0200

    add solution

---
+++ 8 kyu/opposite-number.cs
@@ -0,0 +1,19 @@
+/*Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34*/
+
+//My solution
+
+using System;
+
+public class Kata
+    {
+        public  static int Opposite(int number)
+        {
+          return number * -1;
+        }
+    }
