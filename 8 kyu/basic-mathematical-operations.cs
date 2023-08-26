0485e2197820efb32d98fa233555818c041bb4ed
olivia svensson <oliviateoliva@gmail.com>
2023-08-26 21:16:54 +0200

    add solution

---
+++ 8 kyu/basic-mathematical-operations.cs
@@ -0,0 +1,31 @@
+/*Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7*/
+
+//My solution
+
+namespace Solution
+{
+  public static class Program
+  {
+    public static double basicOp(char operation, double value1, double value2)
+    {
+      if(operation.ToString() == "+") {
+        return value1 + value2;
+      } else if(operation.ToString() == "-") {
+        return value1 - value2;
+      } else if(operation.ToString() == "*") {
+        return value1 * value2;
+      } else {
+        return value1/value2;
+      }
+    }
+  }
+}
