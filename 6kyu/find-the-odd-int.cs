163cd86aadbd421a85a9e5926e1c9d69e1d58c46
olivia svensson <oliviateoliva@gmail.com>
2023-08-26 15:00:06 +0200

    add solution

---
+++ 6kyu/find-the-odd-int.cs
@@ -0,0 +1,42 @@
+/*Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+
+Examples
+[7] should return 7, because it occurs 1 time (which is odd).
+[0] should return 0, because it occurs 1 time (which is odd).
+[1,1,2] should return 2, because it occurs 1 time (which is odd).
+[0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).*/
+
+//My solution
+
+namespace Solution
+{
+  class Kata
+    {
+    public static int find_it(int[] seq) 
+      {
+     int oddInt = 0;		
+		int current = 0;
+      
+        for (int i = 0; i < seq.Length; i++) {
+        	int counter = 0;
+        	current = seq[i];
+        for (int j = 0; j < seq.Length; j++) {
+          	if (seq[j] == current) {
+            	counter++;
+          	} 
+        }
+        if (counter % 2 != 0) {
+          	oddInt = current;
+        } else {
+          	counter = 0;
+        	}
+      	}
+		
+      	return oddInt;
+      }
+       
+    }
+}
